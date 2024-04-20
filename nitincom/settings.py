a8f9f20043fdf454d3dff09102ef28ece1a6d059
Kashee3400 <kasheemilkpcl@gmail.com>
2024-04-20 22:03:59 +0530

    Updated

--- nitincom/settings.py
+++ nitincom/settings.py
@@ -33,6 +33,7 @@ INSTALLED_APPS = [
 ]
 
 MIDDLEWARE = [
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.middleware.security.SecurityMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
@@ -133,14 +134,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
-STATIC_URL = 'static/'
-
 MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
 
 STATIC_URL="static/"
 STATIC_DIR = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT=os.path.join(BASE_DIR,'static/')
+STATIC_ROOT=os.path.join(BASE_DIR,'static')
 STATICFILES_DIRS = [STATIC_DIR]
 
 
