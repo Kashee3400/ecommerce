c37a8ecd1c13d0c223be5b7314e4a127d80f8224
Kashee3400 <kasheemilkpcl@gmail.com>
2024-04-21 09:34:00 +0530

    Updated

--- nitincom/settings.py
+++ nitincom/settings.py
@@ -138,9 +138,7 @@ MEDIA_URL="/media/"
 MEDIA_ROOT=os.path.join(BASE_DIR,"media/")
 
 STATIC_URL="static/"
-STATIC_DIR = os.path.join(BASE_DIR, 'staticfiles')
-STATIC_ROOT=os.path.join(BASE_DIR,'static')
-STATICFILES_DIRS = [STATIC_DIR]
+STATIC_ROOT=os.path.join(BASE_DIR,'static/')
 
 
 # Default primary key field type
